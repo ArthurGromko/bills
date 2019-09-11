2bcd04ecdfa6772dd49694204b8d2e44857a9097
arthurgromko <agv14@hotmail.com>
2019-09-11 16:53:46 -0400

    Page working

---
+++ src/components/BillsTable.vue
@@ -0,0 +1,43 @@
+<template>
+  <table class="m-6 table">
+    <thead class="border-2 border-teal-800 bg-gray-400 text-black">
+      <tr>
+        <th class="border-2 border-teal-800 px-2" scope="col">Date</th>
+        <th class="border-2 border-teal-800 px-2" scope="col">Amount</th>
+        <th class="border-2 border-teal-800 px-2" scope="col">Category</th>
+        <th class="border-2 border-teal-800 px-2" scope="col"></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(bill, index) in bills" :key="index" class="p-4">
+        <td class="border-2 border-teal-800 px-2">{{bill.date | moment("MMM D YYYY")}}</td>
+        <td class="border-2 border-teal-800 px-2">${{bill.amount}}</td>
+        <td class="border-2 border-teal-800 px-2">{{bill.category}}</td>
+        <td class="border-2 border-teal-800 px-2"><button @click="removeBill(index)">𝗫</button></td>
+      </tr>
+      <tr class="p-4 bg-blue-lighter text-center">
+        <td colspan="4"><button class="mt-4 flex-no-shrink p-2 border-2 rounded bg-blue-800 text-white border-teal-800 hover:text-white hover:bg-blue-400" @click="triggerShowAddBill">Add new</button></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import Moment from "moment";
+
+export default {
+  name: 'BillsTable',
+  props: ['bills'],
+    components: {
+        Moment
+    },
+  methods: {
+    triggerShowAddBill: function() {
+      this.$emit('triggerShowAddBill')
+    },
+    removeBill: function(index) {
+      this.$emit('removeBill', index)
+    }
+  }
+}
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul class="list-reset inline flex justify-center border-b-4 mb-0">
+    <li
+      class="p-4 inline hover:bg-gray-300 uppercase font-black cursor-pointer"
+      :class="[ activeCategory === '' ? 'bg-gray-700' : 'bg-gray-500' ]"
+      @click="clearActiveCategory()">
+      All
+    </li>
+    <li
+      class="p-4 inline hover:bg-gray-300 uppercase font-black cursor-pointer"
+      :class="[ activeCategory === category ? 'bg-gray-700' : 'bg-gray-500' ]"
+      v-for="category in categories"
+      :key="category"
+      @click="setActiveCategory(category)" >
+      {{category}}
+    </li>
+
+    <li class="border-l-2 border-gray-900 p-4 inline bg-gray-500 hover:bg-gray-300 uppercase font-black cursor-pointer" @click="triggerShowAddCategory">➕</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['categories', 'activeCategory'],
+  methods: {
+    triggerShowAddCategory() {
+      this.$emit('triggerShowAddCategory')
+    },
+    clearActiveCategory() {
+      this.$emit('clearActiveCategory')
+    },
+    setActiveCategory(category) {
+      this.$emit('setActiveCategory', category)
+    }
+  }
+}
+</script>
